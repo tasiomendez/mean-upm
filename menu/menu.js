a79856fc609e2cdf863729a32432a2e7fdba51bc
Tasio Mendez <tm.tasio@gmail.com>
2020-04-29 15:23:47 +0200

    Icons added

--- menu/menu.js
+++ menu/menu.js
@@ -1,18 +1,17 @@
-let ready = false;
-
 /**
  * Listen for clicks on the buttons, and send the appropriate message to
  * the content script in the page.
  */
 function listenForClicks() {
   document.addEventListener("click", (e) => {
+    e.target.id = e.target.id || e.target.parentNode.id;
 
     function calculator(tabs) {
       browser.tabs.sendMessage(tabs[0].id, {
         function: "calculator"
       });
     }
-
+    
     /**
      * Just log the error to the console.
      */
@@ -25,16 +24,10 @@ function listenForClicks() {
      * Get the active tab,
      * then call the appropriate function.
      */
-    if (e.target.id === "calculator" && ready) {
+    if (e.target.id === "calculator") {
       browser.tabs.query({active: true, currentWindow: true})
         .then(calculator)
         .catch(reportError);
-    }
-    else if (e.target.id === "webmail") {
-      browser.tabs.create({ url: "https://www.upm.es/webmail_alumnos/" });
-    }
-    else if (e.target.id === "polivirtual") {
-      browser.tabs.create({ url: "https://www.upm.es/politecnica_virtual/" });
     }
   });
 };
@@ -47,15 +40,26 @@ function reportExecuteScriptError(error) {
   console.error(`Failed to execute content script: ${error.message}`);
 }
 
-browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  ready = /.*upm\.es\/politecnica\_virtual.*/.test(tabs[0].url)
-});
-
 /**
  * When the popup loads, inject a content script into the active tab,
  * and add a click handler.
  * If we couldn't inject the script, handle the error.
  */
-browser.tabs.executeScript({file: "/background.js"})
-  .then(listenForClicks)
-  .catch(reportExecuteScriptError);
+browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  if(/.*upm\.es\/politecnica\_virtual.*/.test(tabs[0].url))
+    browser.tabs.executeScript({ file: "/background.js" })
+      .then(listenForClicks)
+      .catch(reportExecuteScriptError);
+});
+
+/**
+ * Call the appropriate function when clicking on button.
+ */
+document.addEventListener("click", (e) => {
+  e.target.id = e.target.id || e.target.parentNode.id;
+  if (e.target.id === "webmail") {
+    browser.tabs.create({ url: "https://www.upm.es/webmail_alumnos/" });
+  } else if (e.target.id === "polivirtual") {
+    browser.tabs.create({ url: "https://www.upm.es/politecnica_virtual/" });
+  }
+});
