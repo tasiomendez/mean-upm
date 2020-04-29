4a8a1645bca33ed64cb85d2e7153925f3581e388
Tasio Mendez <tm.tasio@gmail.com>
2020-04-29 14:33:12 +0200

    Improved complexity of code

--- background.js
+++ background.js
@@ -1,40 +1,70 @@
 (function() {
 
+  const RECOGNITION = "R. Cred."
+
+  // Translate column names
+  const translation = {
+    "Asignatura":  "subject",
+    "Año":         "year",
+    "Calif.":      "grade",
+    "Cred.":       "ects",
+    "Conv.":       "session",
+    "Curso":       "course",
+    "Duración":    "duration",
+    "Tipo":        "type"
+  };
+
   /**
-   *
+   * Extract data from the field given. It can be converted to a Number
+   * in case it is needed.
    */
-  function getDataFromRow (row, subjectCol, ectsCol) {
-    let subject = row.children[subjectCol].innerText;
-    let mark = Number(row.lastElementChild.innerText.replace(",",".").match(/[\d[\.|\,]+/));
-    let ects = Number(row.children[ectsCol].innerText.replace(",",".").match(/[\d[\.|\,]+/));
-    return [subject, mark, ects];
+  function getDataFromField (str, toInt) {
+    let number = Number(str.replace(",",".").match(/[\d[\.|\,]+/));
+    if (number && !toInt) return number;
+    else return str;
   }
 
   /**
-   *
+   * Extract data from the row given.
+   */
+  function getDataFromRow (row, cols) {
+    let data = {};
+    for (let i = 0; i < cols.length; i++)
+      data[cols[i]] = getDataFromField(row.children[i].innerText, cols[i] === "subject");
+    return data;
+  }
+
+  /**
+   * Extract data from the table given.
    */
   function getDataFromTable (table) {
-    let cols = Array.from(table.querySelectorAll("thead tr th")).filter((item, index) => {
-      return (item.innerText === "Cred." || item.innerText === "Asignatura");
-    }, []);
+    let cols = getMedataFromTable(table);
     return Array.from(table.querySelectorAll("tbody tr")).map((row) => {
-      let rdata = getDataFromRow(row,
-        Array.prototype.indexOf.call(cols[0].parentNode.children, cols[0]),
-        Array.prototype.indexOf.call(cols[1].parentNode.children, cols[1])
-      );
-      return { subject: rdata[0], mark: rdata[1], ects: rdata[2] }
+      return getDataFromRow(row, cols);
+    }).filter((item) => {
+      return item.grade >= 5.0 || item.grade === RECOGNITION;
     });
   }
 
   /**
-   *
+   * Get the metadata from the table, i.e. the name of the columns and
+   * translate them to be used in other functions.
    */
   function getMedataFromTable (table) {
+    return Array.from(table.querySelectorAll("thead tr th")).map((item) => {
+      return translation[item.innerText];
+    });
+  }
+
+  /**
+   * Get the title of the table.
+   */
+  function getTitleFromTable (table) {
     return table.querySelector("caption").innerText;
   }
 
   /**
-   *
+   * Get HTML template from the extension stored in path.
    */
   function getTemplate (path) {
     return fetch(browser.extension.getURL(path))
@@ -44,48 +74,61 @@
           div.innerHTML = data;
           return div;
         }).catch(error => {
-            console.error(error);
+          console.error(error);
         });
   }
 
   /**
-   *
+   * Builds the average table and is filled with the corresponding data.
+   */
+  function getAverageTable (data) {
+    return getTemplate('/average.html')
+      .then((response) => {
+        response.querySelector("table#average td#weighted").innerText = getWeightedAverage(data);
+        response.querySelector("table#average td#arithmetic").innerText = getArithmeticAverage(data);
+        response.querySelector("table#average td#credits").innerText = getPassedCredits(data);
+        return response;
+      }).catch((error) => {
+        console.log(error);
+      });
+  }
+
+  /**
+   * Extract from the data the weighted mean.
+   * sum(grade * ects) / sum(ects)
    */
   function getWeightedAverage (data) {
-    let acc = data.reduce((acc, item) => {
-      if (item.mark >= 5.0) return acc + (item.mark * item.ects);
-      else return acc;
-    }, 0);
-    let ects = data.reduce((acc, item) => {
-      if (item.mark >= 5.0) return acc + item.ects;
-      else return acc;
-    }, 0);
-    return (acc / ects).toFixed(4);
+    let _data = data.reduce((acc, item) => {
+      if (!Number(item.grade)) return acc;
+      acc.ects += item.ects;
+      acc.acc += item.ects * item.grade;
+      return acc;
+    }, { ects: 0, acc: 0 });
+    return (_data.acc / _data.ects).toFixed(4);
   }
 
   /**
-   *
+   * Extract from the data the arithmetic mean.
+   * sum(grades) / number of subjects
    */
   function getArithmeticAverage (data) {
-    let acc = data.reduce((acc, item) => {
-      if (item.mark >= 5.0) return acc + item.mark;
-      else return acc;
-    }, 0);
-    let subjects = data.reduce((acc, item) => {
-      if (item.mark >= 5.0) return acc + 1;
-      else return acc;
-    }, 0);
-    return (acc / subjects).toFixed(4);
+    let _data = data.reduce((acc, item) => {
+      if (!Number(item.grade)) return acc;
+      acc.subjects++;
+      acc.acc += item.grade;
+      return acc;
+    }, { subjects: 0, acc: 0 });
+    return (_data.acc / _data.subjects).toFixed(4);
   }
 
 
   /**
-   *
+   * Extract from the data the sum off all the ects that the
+   * student has passed.
    */
   function getPassedCredits (data) {
     let ects = data.reduce((acc, item) => {
-      if (item.mark >= 5.0) return acc + item.ects;
-      else return acc;
+      return acc + item.ects;
     }, 0);
     return ects.toFixed(1);
   }
@@ -98,17 +141,19 @@
     let data = Array.from(document.querySelectorAll("table#tabla_expediente[data-role=table]")).reduce((acc, item) => {
       return acc.concat(getDataFromTable(item));
     }, []);
-    getTemplate('/average.html').then((response) => {
+    getAverageTable(data).then((table) => {
       let container = document.querySelectorAll("#contenido_seccion")[2];
       let child = container.firstElementChild;
 
-      response.querySelector("table#average td#weighted").innerText = getWeightedAverage(data);
-      response.querySelector("table#average td#arithmetic").innerText = getArithmeticAverage(data);
-      response.querySelector("table#average td#credits").innerText = getPassedCredits(data);
-
+      // Override if exists
       if (container.querySelector("table#average"))
-        container.children[1].innerHTML = response.innerHTML;
-      else child.parentNode.insertBefore(response, child.nextSibling);
+        container.children[1].innerHTML = table.innerHTML;
+      else child.parentNode.insertBefore(table, child.nextSibling);
+
+      // Make table visible
+      setTimeout(() => {
+        document.querySelector("table#average[data-role=average]").style.backgroundColor = "white";
+      }, 200);
     });
   }
 
--- menu/menu.js
+++ menu/menu.js
@@ -1,3 +1,5 @@
+let ready = false;
+
 /**
  * Listen for clicks on the buttons, and send the appropriate message to
  * the content script in the page.
@@ -23,15 +25,16 @@ function listenForClicks() {
      * Get the active tab,
      * then call the appropriate function.
      */
-    if (e.target.id === "calculator") {
+    if (e.target.id === "calculator" && ready) {
       browser.tabs.query({active: true, currentWindow: true})
         .then(calculator)
         .catch(reportError);
     }
-    else if (e.target.classList.contains("reset")) {
-      browser.tabs.query({active: true, currentWindow: true})
-        .then(reset)
-        .catch(reportError);
+    else if (e.target.id === "webmail") {
+      browser.tabs.create({ url: "https://www.upm.es/webmail_alumnos/" });
+    }
+    else if (e.target.id === "polivirtual") {
+      browser.tabs.create({ url: "https://www.upm.es/politecnica_virtual/" });
     }
   });
 };
@@ -44,14 +47,15 @@ function reportExecuteScriptError(error) {
   console.error(`Failed to execute content script: ${error.message}`);
 }
 
+browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  ready = /.*upm\.es\/politecnica\_virtual.*/.test(tabs[0].url)
+});
+
 /**
  * When the popup loads, inject a content script into the active tab,
  * and add a click handler.
  * If we couldn't inject the script, handle the error.
  */
-browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  if (/.*upm\.es\/politecnica\_virtual.*/.test(tabs[0].url))
-    browser.tabs.executeScript({file: "/background.js"})
-      .then(listenForClicks)
-      .catch(reportExecuteScriptError);
-});
+browser.tabs.executeScript({file: "/background.js"})
+  .then(listenForClicks)
+  .catch(reportExecuteScriptError);
