577822d6ae6191fd4eb95616d6dddbf2fbdc0fb2
Tasio Mendez <tm.tasio@gmail.com>
2020-04-30 02:26:12 +0200

    Statistics page fully working

--- statistics/columns.js
+++ statistics/columns.js
@@ -1,85 +1,70 @@
-(function () {
+/**
+ * Draw a column chart on the element id provided with the data
+ * passed as argument.
+ */
+function drawColumnChart (element, data, _width) {
 
-  /**
-   * Draw a column chart on the element id provided with the data
-   * passed as argument.
-   */
-  function drawColumnChart (element, _data) {
+  // set the dimensions and margins of the graph
+  let margin = {top: 50, right: 50, bottom: 50, left: 50},
+      width = _width - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
 
-    let data = _data.reduce((acc, item) => {
-      acc.push({
-        title: item.title,
-        weighted: Number(getWeightedAverage(item.data)),
-        arithmetic: Number(getArithmeticAverage(item.data))
-      })
-      return acc;
-    }, []).filter((item) => {
-      return !Number.isNaN(Number(item.arithmetic));
-    });
+  // append the svg object to the body of the page
+  let svg = d3.select(element)
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
-    // set the dimensions and margins of the graph
-    let margin = {top: 50, right: 50, bottom: 50, left: 50},
-        width = document.querySelector("#history").clientWidth - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+  // List of subgroups = header of the csv files = soil condition here
+  let subgroups = ["weighted", "arithmetic"];
 
-    // append the svg object to the body of the page
-    let svg = d3.select(element)
-      .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform",
-              "translate(" + margin.left + "," + margin.top + ")");
+  // List of groups = species here = value of the first column called group -> I show them on the X axis
+  let groups = d3.map(data, function(d) { return d.title }).keys();
 
-    // List of subgroups = header of the csv files = soil condition here
-    let subgroups = ["weighted", "arithmetic"];
+  // X axis: scale and draw:
+  let x = d3.scaleBand()
+      .domain(groups)
+      .range([0, width])
+      .padding([0.2]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).tickSize(0).tickPadding(10));
 
-    // List of groups = species here = value of the first column called group -> I show them on the X axis
-    let groups = d3.map(data, function(d) { return d.title }).keys();
+  // Y axis: scale and draw:
+  let y = d3.scaleLinear()
+      .domain([0, 10])
+      .range([height, 0]);
+  svg.append("g")
+      .call(d3.axisLeft(y));
 
-    // X axis: scale and draw:
-    var x = d3.scaleBand()
-        .domain(groups)
-        .range([0, width])
-        .padding([0.2]);
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x).tickSize(0).tickPadding(10));
+  // Another scale for subgroup position?
+  let xSubgroup = d3.scaleBand()
+    .domain(subgroups)
+    .range([0, x.bandwidth()])
+    .padding([0.05]);
 
-    // Y axis: scale and draw:
-    let y = d3.scaleLinear()
-        .domain([0, 10])
-        .range([height, 0]);
-    svg.append("g")
-        .call(d3.axisLeft(y));
+  // color palette = one color per subgroup
+  let color = d3.scaleOrdinal()
+    .domain(subgroups)
+    .range(['#69b3a2','#377eb8']);
 
-    // Another scale for subgroup position?
-    var xSubgroup = d3.scaleBand()
-      .domain(subgroups)
-      .range([0, x.bandwidth()])
-      .padding([0.05]);
-
-    // color palette = one color per subgroup
-    var color = d3.scaleOrdinal()
-      .domain(subgroups)
-      .range(['#69b3a2','#377eb8']);
-
-    // append the bar rectangles to the svg element
-    svg.append("g")
-      .selectAll("g")
-      // Enter in data = loop group per group
-      .data(data)
-      .enter()
-      .append("g")
-        .attr("transform", function(d) { return "translate(" + x(d.title) + ",0)"; })
-      .selectAll("rect")
-      .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
-      .enter().append("rect")
-        .attr("x", function(d) { return xSubgroup(d.key); })
-        .attr("y", function(d) { return y(d.value); })
-        .attr("width", xSubgroup.bandwidth())
-        .attr("height", function(d) { return height - y(d.value); })
-        .attr("fill", function(d) { return color(d.key); });
-  }
-
-})();
+  // append the bar rectangles to the svg element
+  svg.append("g")
+    .selectAll("g")
+    // Enter in data = loop group per group
+    .data(data)
+    .enter()
+    .append("g")
+      .attr("transform", function(d) { return "translate(" + x(d.title) + ",0)"; })
+    .selectAll("rect")
+    .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
+    .enter().append("rect")
+      .attr("x", function(d) { return xSubgroup(d.key); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("width", xSubgroup.bandwidth())
+      .attr("height", function(d) { return height - y(d.value); })
+      .attr("fill", function(d) { return color(d.key); });
+}
--- statistics/statistics.js
+++ statistics/statistics.js
@@ -28,7 +28,6 @@
     return (_data.acc / _data.subjects).toFixed(4);
   }
 
-
   /**
    * Extract from the data the sum off all the ects that the
    * student has passed.
@@ -55,6 +54,41 @@
         });
   }
 
+  /**
+   * Append a new row to the table given with the data passed as parameter.
+   * The title of the row is passed as parameter and it could be passed
+   * an extra class to the <tr> element.
+   */
+  function fillAverageTableRow (table, data, title, className) {
+
+    let $tr = document.createElement("tr");
+    if (className)
+      $tr.className = className;
+    let $th = document.createElement("th");
+    $th.setAttribute("scope", "row");
+    $th.innerText = title;
+    let $ma = document.createElement("td");
+    $ma.className = "center";
+    $ma.innerText = getArithmeticAverage(data);
+    let $mp = document.createElement("td");
+    $mp.className = "center";
+    $mp.innerText = getWeightedAverage(data);
+    let $cs = document.createElement("td");
+    $cs.className = "center";
+    $cs.innerText = getPassedCredits(data);
+
+    $tr.appendChild($th);
+    $tr.appendChild($ma);
+    $tr.appendChild($mp);
+    $tr.appendChild($cs);
+
+    table.querySelector("tbody").appendChild($tr);
+    return table;
+  }
+
+  /**
+   * Get the overall template and fill it.
+   */
   function getOverallTemplate (data, career) {
     let total = Array.from(data).reduce((acc, item) => {
       return acc.concat(item.data);
@@ -62,58 +96,93 @@
     return getTemplate('/statistics/modules/overall.html')
         .then((template) => {
           template.querySelector(".career-name").innerText = "Titulación en " + career;
+          let table = template.querySelector(".overall-average");
 
           for (let i = 0; i < data.length; i++) {
-            let $tr = document.createElement("tr");
-            let $th = document.createElement("th");
-            $th.setAttribute("scope", "row");
-            $th.innerText = data[i].title;
-            let $ma = document.createElement("td");
-            $ma.className = "center";
-            $ma.innerText = getArithmeticAverage(data[i].data);
-            let $mp = document.createElement("td");
-            $mp.className = "center";
-            $mp.innerText = getWeightedAverage(data[i].data);
-            let $cs = document.createElement("td");
-            $cs.className = "center";
-            $cs.innerText = getPassedCredits(data[i].data);
-
-            $tr.appendChild($th).appendChild($ma).appendChild($mp).appendChild($cs);
-
-            template.querySelector(".overall-average tbody").appendChild($tr);
+            fillAverageTableRow(table, data[i].data, data[i].title);
           }
 
-          let $tr = document.createElement("tr");
-          $tr.className = "total";
-          let $th = document.createElement("th");
-          $th.setAttribute("scope", "row");
-          $th.innerText = "Total";
-          let $ma = document.createElement("td");
-          $ma.className = "center";
-          $ma.innerText = getArithmeticAverage(total);
-          let $mp = document.createElement("td");
-          $mp.className = "center";
-          $mp.innerText = getWeightedAverage(total);
-          let $cs = document.createElement("td");
-          $cs.className = "center";
-          $cs.innerText = getPassedCredits(total);
-
-          $tr.appendChild($th).appendChild($ma).appendChild($mp).appendChild($cs);
-
-          template.querySelector(".overall-average tbody").appendChild($tr);
-
+          fillAverageTableRow(table, total, "Total", "total");
           return template;
+
         }).then((template) => {
           document.querySelector(".module-wrapper").innerHTML = template.innerHTML;
+
         }).then((template) => {
+          let width = document.querySelector("#columnchart").clientWidth;
 
-          drawHistogram(total);
-          drawColumnChart(data);
+          drawHistogram("#histogramchart", total, width);
 
+          // Fit data to draw the column chart
+          let _data = data.reduce((acc, item) => {
+            acc.push({
+              title: item.title,
+              weighted: Number(getWeightedAverage(item.data)),
+              arithmetic: Number(getArithmeticAverage(item.data))
+            })
+            return acc;
+          }, []).filter((item) => {
+            return !Number.isNaN(Number(item.arithmetic));
+          });
+
+          drawColumnChart('#columnchart', _data, width);
         });
   }
 
-  function setSidebarMenu(data, career) {
+  /**
+   * Append a new menu entry on the sidebar menu.
+   */
+  function buildNewMenuItem (title, i) {
+    let $li = document.createElement("li");
+    $li.className = "nav-item";
+
+    let $div = document.createElement("div");
+    $div.className = "nav-link";
+    $div.setAttribute("id", "tab" + i + "-tab");
+    $div.setAttribute("data-toggle", "tab");
+    $div.setAttribute("href", "#tab" + i);
+    $div.setAttribute("role", "tab");
+    $div.setAttribute("aria-controls", "tab" + i);
+    $div.setAttribute("aria-selected", "false");
+
+    let $img = document.createElement("img");
+    $img.setAttribute("src", "/icons/calendar@black.svg");
+    $img.setAttribute("alt", "icon");
+
+    let $span = document.createElement("span");
+    $span.className = "media-body";
+    $span.innerText = title;
+
+    $div.appendChild($img);
+    $div.appendChild($span);
+    $li.appendChild($div);
+    return $li;
+  }
+
+  /**
+   * Build a new pane which is connected to a menu item.
+   */
+  function buildNewPane (template, i) {
+    // Tab panels
+    let $pane = document.createElement("div");
+    $pane.className = "tab-pane fade";
+    $pane.setAttribute("id", "tab" + i);
+    $pane.setAttribute("role", "tabpanel");
+    $pane.setAttribute("aria-labelledby", "tab" + i + "-tab");
+
+    let $wrapper = document.createElement("div");
+    $wrapper.className = "col-sm-10 offset-sm-1 module-wrapper";
+
+    $wrapper.appendChild(template);
+    $pane.appendChild($wrapper);
+    return $pane;
+  }
+
+  /**
+   * Fill the sidebar menu and creates the necessary
+   * panes connected to them;
+   */
+  function setSidebarMenu (data, career) {
     let menu = document.querySelector("ul.pmd-sidebar-nav");
     while (menu.childNodes.length > 2)
         menu.removeChild( menu.lastChild );
@@ -121,74 +190,35 @@
     for (let i = 0; i < data.length; i++) {
       if (!data[i].title) continue;
 
-      let $li = document.createElement("li");
-      $li.className = "nav-item";
-      let $div = document.createElement("div");
-      $div.className = "nav-link";
-      $div.setAttribute("id", "tab" + i + "-tab");
-      $div.setAttribute("data-toggle", "tab");
-      $div.setAttribute("href", "#tab" + i);
-      $div.setAttribute("role", "tab");
-      $div.setAttribute("aria-controls", "tab" + i);
-      $div.setAttribute("aria-selected", "false");
-      let $img = document.createElement("img");
-      $img.setAttribute("src", "/icons/calendar@black.svg");
-      $img.setAttribute("alt", "icon");
-      let $span = document.createElement("span");
-      $span.className = "media-body";
-      $span.innerText = data[i].title;
-
-      $div.appendChild($img);
-      $div.appendChild($span);
-      $li.appendChild($div);
-
+      let $li = buildNewMenuItem(data[i].title, i);
       menu.appendChild($li);
 
       getTemplate('/statistics/modules/year.html')
           .then((template) => {
             template.querySelector(".career-name").innerText = "Titulación en " + career;
 
-            // Tab panels
-            let $pane = document.createElement("div");
-            $pane.className = "tab-pane fade";
-            $pane.setAttribute("id", "tab" + i);
-            $pane.setAttribute("role", "tabpanel");
-            $pane.setAttribute("aria-labelledby", "tab" + i + "-tab");
-            let $wrapper = document.createElement("div");
-            $wrapper.className = "col-sm-10 offset-sm-1 module-wrapper";
-
-            $wrapper.appendChild(template);
-            $pane.appendChild($wrapper);
+            let $pane = buildNewPane(template, i);
             document.querySelector(".main-content").appendChild($pane);
 
-            return template;
+            let table = template.querySelector(".overall-average");
+            fillAverageTableRow(table, data[i].data, data[i].title);
 
-          }).then((template) => {
-
-            let $tr = document.createElement("tr");
-            let $th = document.createElement("th");
-            $th.setAttribute("scope", "row");
-            $th.innerText = data[i].title;
-            let $ma = document.createElement("td");
-            $ma.className = "center";
-            $ma.innerText = getArithmeticAverage(data[i].data);
-            let $mp = document.createElement("td");
-            $mp.className = "center";
-            $mp.innerText = getWeightedAverage(data[i].data);
-            let $cs = document.createElement("td");
-            $cs.className = "center";
-            $cs.innerText = getPassedCredits(data[i].data);
-
-            $tr.appendChild($th);
-            $tr.appendChild($ma);
-            $tr.appendChild($mp);
-            $tr.appendChild($cs);
-
-            template.querySelector(".overall-average tbody").appendChild($tr);
+            template.querySelector("#histogramchart").setAttribute("id", "histogramchart" + i);
           });
+
+      // Draw histogram every time menu changes
+      $('div[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        document.querySelector("#histogramchart" + i).innerHTML = "";
+        let width = document.querySelector("#histogramchart" + i).clientWidth;
+        drawHistogram("#histogramchart" + i, data[i].data, width);
+      });
     }
   }
 
+  /**
+   * Add a listener to receive messages. The message receive contains
+   * the data which is shown.
+   */
   browser.runtime.onMessage.addListener((message) => {
     let career = message.data.shift();
     document.querySelectorAll(".hidden").forEach((item) => {
